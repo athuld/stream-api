8106a050e9cb765dc33cc422a7ed145bcbffe932
Athul Dinesan <athuldinesan5@gmail.com>
2023-03-19 23:21:38 +0530

    bot: add hash search and optimise code
    Signed-off-by: Athul Dinesan <athuldinesan5@gmail.com>

--- app/router.go
+++ app/router.go
@@ -2,9 +2,9 @@ package app
 
 import "streamapi/controllers"
 
-
-func mapUrls(){
-  router.GET("/",controllers.HomeHello)
-  router.POST("/add",controllers.AddStreamData)
-  router.GET("/get",controllers.SearchData)
+func mapUrls() {
+	router.GET("/", controllers.HomeHello)
+	router.POST("/add", controllers.AddStreamData)
+	router.GET("/get", controllers.SearchData)
+	router.GET("/file", controllers.GetFileData)
 }
